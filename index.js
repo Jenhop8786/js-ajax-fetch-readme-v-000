22827d544d00f54288aea824a9bcdda5d4359356
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-20 00:38:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+const token = 'YOUR_TOKEN_HERE'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
