1daf2071ec6219a336b3ddc3c3b7e98f97d0c315
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-20 00:37:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
 const token = 'd7b4691f973e11e187650283b11a8e5f169146'
-fetch('https://api.github.com/user/repos', {
-  headers: {
-    Authorization: `token ${token}`
-  }
-}).then(res => res.json()).then(json => console.log(json));
