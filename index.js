28e5b9d9acc7de2d9d2fa894552c90f8c1cab9d0
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-20 00:36:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const app = "I don't do much.";
-
-fetch('https://api.github.com/repos/jquery/jquery/commits')
-  .then(res => res.json())
-  .then(json => console.log(json));
+const token = 'd7b4691f973e11e187650283b11a8e5f169146'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
